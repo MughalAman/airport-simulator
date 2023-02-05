c5129ed44f180aa959949f1551cf2664fc58c80e
Jeremia <103106203+Xarros0@users.noreply.github.com>
2023-02-05 16:32:02 +0200

    Testi
    pohjatesti

--- src/testi/AsiakasTest.java
+++ src/testi/AsiakasTest.java
@@ -7,22 +7,9 @@ import simu.model.Asiakas;
 
 import static org.junit.Assert.assertEquals;
 
-
-
 public class AsiakasTest {
-	  @Test
-	  public void testAsiakkaanSaapuminen() {
-	    Asiakas asiakas = new Asiakas();
-	    asiakas.setSaapumisaika(1000);
-
-	    assertEquals(1000, asiakas.getSaapumisaika());
-	  }
-
-	  @Test
-	  public void testAsiakkaanPoistuminen() {
-	    Asiakas asiakas = new Asiakas();
-	    asiakas.setPoistumisaika(2000);
-
-	    assertEquals(2000, asiakas.getPoistumisaika());
-	  }
+	@Test
+	void test() {
+		fail("Not yet implemented");
 	}
+}
