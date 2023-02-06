8086b3f11502faa8863205f95cdb5fdd54a93c6f
Jeremia <103106203+Xarros0@users.noreply.github.com>
2023-02-07 00:20:59 +0200

    Asiakkaan testaus

--- src/testi/AsiakasTest.java
+++ src/testi/AsiakasTest.java
@@ -4,12 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import simu.model.Asiakas;
+import simu.framework.Kello;
+import simu.framework.Trace;
 
 import static org.junit.Assert.assertEquals;
 
 public class AsiakasTest {
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testAsiakasProperties() {
+    	Trace.setTraceLevel(Trace.Level.INFO);
+        Asiakas asiakas = new Asiakas();
+        double saapumisaika = Kello.getInstance().getAika();
+        asiakas.setPoistumisaika(saapumisaika + 5);
+        
+        assertEquals(saapumisaika, asiakas.getSaapumisaika(), 0.001);
+        assertEquals(saapumisaika + 5, asiakas.getPoistumisaika(), 0.001);
+    }
 }
