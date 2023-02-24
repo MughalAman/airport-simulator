05f0574ac4429f428e397ade7025e2d408251a36
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2023-02-24 12:02:40 +0200

    Jakaumat Amin

--- src/simu/model/OmaMoottori.java
+++ src/simu/model/OmaMoottori.java
@@ -1,26 +1,27 @@
 package simu.model;
 
+import controller.IKontrolleri;
+import eduni.distributions.Bernoulli;
 import eduni.distributions.Negexp;
 import eduni.distributions.Normal;
-import eduni.distributions.Binomial;
-import eduni.distributions.Bernoulli;
 import simu.framework.Kello;
 import simu.framework.Moottori;
 import simu.framework.Saapumisprosessi;
 import simu.framework.Tapahtuma;
 
+
 public class OmaMoottori extends Moottori{
 	
 	private Saapumisprosessi saapumisprosessi;
-
-	private double C, B, T, Ri, W;
+	
 	private double B, T, Ri, W;
 	private int C = 0;
 	private double checkinJakauma = 0.5, sisaankayntiJakauma = 0.5; // Jakaumat oletuksena 50/50
 	
-	
-	public OmaMoottori(){
-			
+	public OmaMoottori(IKontrolleri kontrolleri){ // UUSI
+
+		super(kontrolleri); //UUSI
+		
 		palvelupisteet = new Palvelupiste[7];
 	
 		palvelupisteet[0]=new Palvelupiste(new Normal(5,3), tapahtumalista, TapahtumanTyyppi.INFO);
@@ -41,24 +42,10 @@ public class OmaMoottori extends Moottori{
 		saapumisprosessi.generoiSeuraava(); // Ensimmäinen saapuminen järjestelmään
 	}
 	
+	
 	@Override
 	protected void suoritaTapahtuma(Tapahtuma t){  // B-vaiheen tapahtumat
 
-	    Asiakas a;
-	    switch (t.getTyyppi()){
-	        
-	        case ENTRANCE: palvelupisteet[0].lisaaJonoon(new Asiakas());      
-	                   saapumisprosessi.generoiSeuraava();    
-	            break;
-	        case INFO: a = palvelupisteet[0].otaJonosta();
-	                    palvelupisteet[1].lisaaJonoon(a);
-	            break;
-	        case CHECKINAUTO: a = palvelupisteet[1].otaJonosta();
-	                    palvelupisteet[2].lisaaJonoon(a); 
-	            break;  
-	        case CHECKINMANUAL: a = palvelupisteet[2].otaJonosta();
-	                    palvelupisteet[3].lisaaJonoon(a); 
-	            break;
 		Asiakas a;
 		switch (t.getTyyppi()){
 			
@@ -100,33 +87,34 @@ public class OmaMoottori extends Moottori{
 			case CHECKINMANUAL: a = palvelupisteet[2].otaJonosta();
             		palvelupisteet[3].lisaaJonoon(a); 
             	break;
-     
+            	
 	        case SECURITY:a = palvelupisteet[3].otaJonosta();
-	                    palvelupisteet[4].lisaaJonoon(a); 
-	            break;
-	            
-	        case SECURITYGATE:a = palvelupisteet[4].otaJonosta();
-	                    palvelupisteet[5].lisaaJonoon(a); 
-	            break;
-	            
+            		palvelupisteet[4].lisaaJonoon(a); 
+            	break;
+            	
+	        case SECURITYGATE: a = palvelupisteet[4].otaJonosta();
+            		palvelupisteet[5].lisaaJonoon(a); 
+            	break;
+    
 	        case GATE:a = palvelupisteet[5].otaJonosta();
-	                    palvelupisteet[6].lisaaJonoon(a); 
-	                break;
-	        
+            		palvelupisteet[6].lisaaJonoon(a); 
+            	break;
+
 	        case PLANE:
-	             a = palvelupisteet[6].otaJonosta();
-	               a.setPoistumisaika(Kello.getInstance().getAika());
-	               a.raportti(); 
-	               C++;
-	            break;
-	            
-	    }   
+	        		a = palvelupisteet[6].otaJonosta();
+	        		a.setPoistumisaika(Kello.getInstance().getAika());
+	        		C++;
+	        		a.raportti();
+	        	break;
+		}	
 	}
 
-
-	
 	@Override
-	protected void tulokset() {	
+	public void tulokset() {
+		
+		// VANHAA tekstipohjaista
+		// System.out.println("Simulointi päättyi kello " + Kello.getInstance().getAika());
+		// System.out.println("Tulokset ... puuttuvat vielä");
 		T = Kello.getInstance().getAika();
 		System.out.println("T: "+T);
 		B = palvelupisteet[1].getAktiiviaika() + palvelupisteet[2].getAktiiviaika(); 
@@ -147,17 +135,30 @@ public class OmaMoottori extends Moottori{
 		System.out.println("Aika asiakkaan turvatarkastuksen jonoon saapumisesta turvatarkastuksen päättymiseen: "+(W/C));
 		System.out.println("Turvatarkastuksen keskimääräinen jononpituus: "+(W/T));
 		
+		// UUTTA graafista
+		kontrolleri.naytaLoppuaika(Kello.getInstance().getAika());
+		
+		String 	tulosStr = "Simulointi päättyi kello " + Kello.getInstance().getAika() + "\n" +  "Tulokset: " + "\n" + "Määrä asiakkaita, jotka pääsivät lentokoneeseen: "+C + "\n"  +  "Check-in aktiiviaika: "+B + "\n"
+				+ "Simuloinnin kokonaisaika: "+T + "\n"	+ "Check-in käyttöaste: "+(B/T) + "\n" + "Lentokentän suoritusteho: "+(C/T) + "\n" + "Check-in keskimääräinen palveluaika: "+(B/C) +  "\n" + "Aika asiakkaan turvatarkastuksen jonoon saapumisesta turvatarkastuksen päättymiseen: "+Ri + "\n"+
+				"Kokonaisoleskeluaika turvatarkastuksessa. Tämä on asiakkaiden läpimenoaikojen summa turvatarkastuksesta: "+W + "\n"
+				+ "Aika asiakkaan turvatarkastuksen jonoon saapumisesta turvatarkastuksen päättymiseen: "+(W/C) + "\n" + "Turvatarkastuksen keskimääräinen jononpituus: "+(W/T);
+		
+		
+		kontrolleri.naytaLoppuTuloste(tulosStr);
+		
+		
 	}
 
 	// Sisäänkäyti jakauma INFO/Check-in
 	public void setSisaankayntiJakauma(double jakauma) {
 		sisaankayntiJakauma = jakauma;
 	}
-	
+		
 	// Check-in jakauma CHECKINAUTO/CHECKIMANUAL
 	public void setCheckinJakauma(double jakauma) {
 		checkinJakauma = jakauma;
 	}
 
+	
 	
 }
