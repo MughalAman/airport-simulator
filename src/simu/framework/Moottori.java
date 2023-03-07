f4e04dae71bc8b5a326741bcc5f4837f33f567cc
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2023-03-07 18:47:49 +0200

    Visualisointi

--- src/simu/framework/Moottori.java
+++ src/simu/framework/Moottori.java
@@ -5,46 +5,46 @@ import simu.model.Palvelupiste;
 import simu.model.Palvelupisteryhma;
 
 public abstract class Moottori extends Thread implements IMoottori{  // UUDET MÄÄRITYKSET
-
+	
 	private double simulointiaika = 0;
 	private long viive = 0;
-
+	
 	private Kello kello;
-
+	
 	protected Tapahtumalista tapahtumalista;
 	protected Palvelupisteryhma[] palvelupisteet;
-
+	
 	protected IKontrolleri kontrolleri; // UUSI
-
+	
 
 	public Moottori(IKontrolleri kontrolleri){  // UUSITTU
-
+		
 		this.kontrolleri = kontrolleri;  //UUSI
 
 		kello = Kello.getInstance(); // Otetaan kello muuttujaan yksinkertaistamaan koodia
-
+		
 		tapahtumalista = new Tapahtumalista();
-
-		// Palvelupisteet luodaan simu.model-pakkauksessa Moottorin aliluokassa
-
-
+		
+		// Palvelupisteet luodaan simu.model-pakkauksessa Moottorin aliluokassa 
+		
+		
 	}
 
 	@Override
 	public void setSimulointiaika(double aika) {
 		simulointiaika = aika;
 	}
-
+	
 	@Override // UUSI
 	public void setViive(long viive) {
 		this.viive = viive;
 	}
-
-	@Override // UUSI
+	
+	@Override // UUSI 
 	public long getViive() {
 		return viive;
 	}
-
+	
 	@Override
 	public void run(){ // Entinen aja()
 		alustukset(); // luodaan mm. ensimmäinen tapahtuma
@@ -55,9 +55,9 @@ public abstract class Moottori extends Thread implements IMoottori{  // UUDET M�
 			yritaCTapahtumat();
 		}
 		tulokset();
-
+		
 	}
-
+	
 	private void suoritaBTapahtumat(){
 		while (tapahtumalista.getSeuraavanAika() == kello.getAika()){
 			suoritaTapahtuma(tapahtumalista.poista());
@@ -74,17 +74,17 @@ public abstract class Moottori extends Thread implements IMoottori{  // UUDET M�
 		}
 	}
 
-
+	
 	private double nykyaika(){
 		return tapahtumalista.getSeuraavanAika();
 	}
-
+	
 	private boolean simuloidaan(){
 		Trace.out(Trace.Level.INFO, "Kello on: " + kello.getAika());
 		return kello.getAika() < simulointiaika;
 	}
-
-
+	
+			
 	private void viive() { // UUSI
 		Trace.out(Trace.Level.INFO, "Viive " + viive);
 		try {
@@ -95,9 +95,10 @@ public abstract class Moottori extends Thread implements IMoottori{  // UUDET M�
 	}
 
 	protected abstract void alustukset(); // Määritellään simu.model-pakkauksessa Moottorin aliluokassa
-
+	
 	protected abstract void suoritaTapahtuma(Tapahtuma t);  // Määritellään simu.model-pakkauksessa Moottorin aliluokassa
-
+	
 	protected abstract void tulokset(); // Määritellään simu.model-pakkauksessa Moottorin aliluokassa
+	
 
 }
