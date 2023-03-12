d5f31eaf47194bf18e137f72f02f1924d80982d5
AminSalamatin <115285429+AminSalamatin@users.noreply.github.com>
2023-03-12 21:02:25 +0200

    Palvelupiste ja Asiakas javadoc

--- src/simu/model/Asiakas.java
+++ src/simu/model/Asiakas.java
@@ -17,28 +17,61 @@ Tämä luokka määrittää asiakkaan metodit ja muuttuja
 	private TapahtumanTyyppi tyyppi = TapahtumanTyyppi.ENTRANCE;
 
 	
+	/**
+	 * Luo uuden asiakkaan, asettaa saapumisajan ja tulostaa tiedon luomisesta.
+	 */
+	
 	public Asiakas(){
 	    id = i++;
 	    
 		saapumisaika = Kello.getInstance().getAika();
 		Trace.out(Trace.Level.INFO, "Uusi asiakas:" + id + ":"+saapumisaika);
 	}
+	
+	
+	/**
+	 * Palauttaa asiakkaan poistumisajan.
+	 * @return Asiakkaan poistumisaika.
+	 */
 
 	public double getPoistumisaika() {
 		return poistumisaika;
 	}
+	
+	
+	/**
+	 * Asettaa asiakkaan poistumisajan.
+	 * @param poistumisaika Asiakkaan poistumisaika.
+	 */
 
 	public void setPoistumisaika(double poistumisaika) {
 		this.poistumisaika = poistumisaika;
 	}
+	
+	
+	/**
+	 * Palauttaa asiakkaan saapumisajan.
+	 * @return Asiakkaan saapumisaika.
+	 */
 
 	public double getSaapumisaika() {
 		return saapumisaika;
 	}
+	
+	
+	/**
+	 * Asettaa asiakkaan saapumisajan.
+	 * @param saapumisaika Asiakkaan saapumisaika.
+	 */
 
 	public void setSaapumisaika(double saapumisaika) {
 		this.saapumisaika = saapumisaika;
 	}
+	
+	
+	/**
+	 * Tulostaa raportin asiakkaan saapumis- ja poistumisajasta sekä viipymisajasta ja asiakkaiden läpimenojen keskiarvosta.
+	 */
 	
 	public void raportti(){
 		Trace.out(Trace.Level.INFO, "Asiakas "+id+ " saapui:" +saapumisaika);
@@ -49,9 +82,21 @@ Tämä luokka määrittää asiakkaan metodit ja muuttuja
 		System.out.println("Asiakkaiden läpimenoaikojen keskiarvo "+ keskiarvo);
 	}
 	
+	
+	/**
+	 * Asettaa asiakkaan tapahtuman tyypin.
+	 * @param tyyppi Tapahtuman tyyppi.
+	 */
+	
 	public void setTapahtuma(TapahtumanTyyppi tyyppi) {
 		this.tyyppi = tyyppi;
 	}
+	
+	
+	/**
+	 * Palauttaa asiakkaan tapahtuman tyypim.
+	 * @return Tapahtuman tyyppi.
+	 */
 	
 	public TapahtumanTyyppi getTapahtuma() {
 		return tyyppi;
--- src/simu/model/Palvelupiste.java
+++ src/simu/model/Palvelupiste.java
@@ -7,9 +7,14 @@ import simu.framework.Tapahtuma;
 import simu.framework.Tapahtumalista;
 
 /**
-@author Jeremia Kekkonen, Aman Mughal ja Amin Salamatin
-@version 2.0
+*Palvelupiste-luokka mallintaa palvelupistettä, jossa asiakkaat saavat palvelua.
+*Palvelupisteellä on jono, joka toimii tietorakenteena. Palvelupisteellä on myös
+*palveluaika, joka arvotaan asiakkaan palvelun alkaessa, sekä aktiivi- ja oleskeluaika,
+*jotka kertovat palvelupisteen käyttöasteen.
+*@author Jeremia Kekkonen, Aman Mughal ja Amin Salamatin
+*@version 2.0
 */
+
 public class Palvelupiste implements Comparable<Palvelupiste>{
 
 	private LinkedList<Asiakas> jono = new LinkedList<Asiakas>(); // Tietorakennetoteutus
@@ -27,17 +32,35 @@ public class Palvelupiste implements Comparable<Palvelupiste>{
 	
 	private int id = 0;
 	
+	/**
+	 * Konstruktori luo uuden Palvelupiste-olion annetuilla parametreilla.
+	 * @param generator - generoi palveluajan
+	 * @param tapahtumalista - tapahtumalista
+	 * @param tyyppi - tapahtuman tyyppi
+	 */
+	
 	public Palvelupiste(ContinuousGenerator generator, Tapahtumalista tapahtumalista, TapahtumanTyyppi tyyppi){
 		this.tapahtumalista = tapahtumalista;
 		this.generator = generator;
 		this.skeduloitavanTapahtumanTyyppi = tyyppi;
 				
 	}
-
+	
+	
+	/**
+	 * Lisää uuden asiakkaan jonoon.
+	 * @param a - uusi asiakas
+	 */
 
 	public void lisaaJonoon(Asiakas a){   // Jonon 1. asiakas aina palvelussa
 		jono.add(a);
 	}
+	
+	
+	/**
+	 * Palauttaa jonon ensimmäisen asiakkaan ja poistaa sen jonosta.
+	 * @return palvelussa ollut asiakas
+	 */
 
 	public Asiakas otaJonosta(){  // Poistetaan palvelussa ollut
 		Asiakas a = jono.poll();
@@ -47,29 +70,62 @@ public class Palvelupiste implements Comparable<Palvelupiste>{
 	}
 
 	
+	/**
+	 * Aloitetaan uusi palvelu, jolloin asiakas on jonossa palvelun aikana.
+	 */
+	
 	public void aloitaPalvelu(){  //Aloitetaan uusi palvelu, asiakas on jonossa palvelun aikana
 		varattu = true;
 		double palveluaika = generator.sample();
 		aktiiviaika += palveluaika;
 		tapahtumalista.lisaa(new Tapahtuma(skeduloitavanTapahtumanTyyppi,Kello.getInstance().getAika()+palveluaika));
 	}
-
+	
+	
+	/**
+	*Kertoo, onko palvelupiste varattu vai ei.
+	*@return True, jos varattu. False, jos ei varattu.
+	*/
 
 	public boolean onVarattu(){
 		return varattu;
 	}
-
+	
+	
+	/**
+	*Kertoo, onko palvelupisteessä jonoa vai ei.
+	*@return True, jos jonossa on asiakkaita. False, jos ei.
+	*/
+	
 	public boolean onJonossa(){
 		return jono.size() != 0;
 	}
 	
+	/**
+	*Palauttaa palvelun aktiiviajan.
+	*@return Palvelun aktiiviaika.
+	*/
+
+	
 	public int getAktiiviaika() {
 		return aktiiviaika;
 	}
+	
+	
+	/**
+	*Palauttaa palvelun oleskeluajan.
+	*@return Palvelun oleskeluaika.
+	*/
 
 	public int getOleskeluaika() {
 		return oleskeluaika;
 	}
+	
+	
+	/**
+	*Palauttaa palvelun aktiivisuusajan.
+	*@return Palvelun aktiivisuusaika.
+	*/
 
 	public int getViiveaika() {
 		return oleskeluaika-aktiiviaika;
