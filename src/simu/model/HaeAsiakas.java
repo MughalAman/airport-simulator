6797721c80e0d10e81246b9654de3ced1c888549
trzero0 <84072977+trzero0@users.noreply.github.com>
2023-03-12 16:08:28 +0200

    g

--- src/simu/model/HaeAsiakas.java
+++ src/simu/model/HaeAsiakas.java
@@ -1,15 +1,27 @@
 package simu.model;
 
+/**
+ * @author 
+ *HaeAsiakas-luokka, joka kuvaa tapahtumia, joilla haetaan asiakkaita tiettyyn tapahtumaan.
+ */
 public class HaeAsiakas {
 
 	private TapahtumanTyyppi tyyppi;
 
+	/**
+	 * Asettaa haettavan tapahtuman tyypin.
+	 * 
+	 * @param tyyppi haettavan tapahtuman tyyppi
+	 */
 	public void setTapahtuma(TapahtumanTyyppi tyyppi) {
 		this.tyyppi = tyyppi;
-
 	}
 
-
+	/**
+	 * Palauttaa haettavan tapahtuman tyypin.
+	 * 
+	 * @return haettavan tapahtuman tyyppi
+	 */
 	public TapahtumanTyyppi getTapahtuma() {
 		return tyyppi;
 	}
--- src/simu/model/ITulosDAO.java
+++ src/simu/model/ITulosDAO.java
@@ -1,6 +1,24 @@
 package simu.model;
 
+/**
+*@author 
+*Rajapinta tulosten tallennus- ja lukemistoiminnoille.
+*/
+
 public interface ITulosDAO {
+	
+	
+	/**
+	*Tallentaa uuden tuloksen tietokantaan.
+	*@param tulos tallennettava Tulos-olio
+	*@return true, jos tallennus onnistui, muuten false
+	*/
 	public boolean createTulos(Tulos tulos);
+	
+	
+	/**
+	*Lukee tietokannasta tallennetut tulokset.
+	*@return Tulos-taulukko, joka sisältää kaikki tallennetut tulokset
+	*/
 	public Tulos[] readTulokset();
 }
