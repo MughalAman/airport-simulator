233162b838275a22cc3fc8e3f7323a7421cdb7a0
Jeremia <103106203+Xarros0@users.noreply.github.com>
2023-03-12 15:32:11 +0200

    Java doc kommentointi controlleri pakettiin

--- src/controller/IKontrolleri.java
+++ src/controller/IKontrolleri.java
@@ -2,31 +2,136 @@ package controller;
 
 import simu.model.TapahtumanTyyppi;
 
+/**
+Rajapinta, joka määrittelee käyttöliittymän ja moottorin väliset toiminnot.
+
+Käyttöliittymä voi käyttää tätä rajapintaa käynnistääkseen simuloinnin, muuttaakseen simuloinnin nopeutta, tallentaakseen tuloksia
+
+ja näyttääkseen tuloksia.
+
+Moottori voi käyttää tätä rajapintaa välittääkseen tietoja simuloinnin tilasta käyttöliittymälle.
+
+Tämän rajapinnan toteuttavan luokan pitää toteuttaa kaikki rajapinnassa määritellyt metodit.
+
+@author [tekijä]
+@version [versionumero]
+*/
+
 public interface IKontrolleri {
 
 		// Rajapinta, joka tarjotaan  käyttöliittymälle:
 
+    	// Käynnistää simuloinnin.
 		public void kaynnistaSimulointi();
+		
+		// Nopeuttaa simulointia.
 		public void nopeuta();
+		
+		// Hidastaa simulointia.
 		public void hidasta();
+		
+		/**
+	    Tallentaa tuloksia simuloinnin loputtua.
+	    
+	    @param Loppuaika Simuloinnin lopetusaika.
+	    @param AsiakasMaara Asiakkaiden määrä simuloinnin aikana.
+	    @param CheckAktiiviAika Check-in-pisteiden aktiiviaika simuloinnin aikana.
+	    @param TurvaTarkastus Turvatarkastusjonon pituus simuloinnin aikana.
+	    @param OleskeluAikaTurvaTarkastus Turvatarkastuksen läpäisseiden asiakkaiden keskimääräinen oleskeluaika turvatarkastusjonossa.
+	    @param CheckInKayttoaste Check-in-pisteiden käyttöaste simuloinnin aikana.
+	    @param LentokentanTeho Lentokentän tehokkuus simuloinnin aikana.
+	    @param CheckinPalveluaika Check-in-pisteiden palveluaika.
+	    @param TurvatarkastusJono Turvatarkastusjonon pituus simuloinnin aikana.
+	    @param TurvatarkastusJononPituus Turvatarkastusjonon pituus simuloinnin aikana.
+	    */
 		public void tallennaTulokset(double Loppuaika, int AsiakasMaara, double CheckAktiiviAika, double TurvaTarkastus, double OleskeluAikaTurvaTarkastus, double CheckInKayttoaste, double LentokentanTeho, double CheckinPalveluaika, double TurvatarkastusJono, double TurvatarkastusJononPituus);
+		
+		/**
+	    Palauttaa tallennetut tulokset merkkijonomuodossa.
+	    
+	    @return Merkkijonomuotoinen esitys tallennetuista tuloksista.
+	    */
 		public abstract String[] naytaTulokset();
 
 		// Rajapinta, joka tarjotaan moottorille:
-
+		
+		/**
+	    Näyttää simuloinnin lopetushetken.
+	    
+	    @param aika Simuloinnin lopetushetki.
+	    */
 		public void naytaLoppuaika(double aika);
-		//public void visualisoiAsiakas();
+		// public void visualisoiAsiakas();
 
+		/**
+	    Asettaa asiakasmäärän käyttöliittymässä näytettäväksi.
+	    
+	    @param C Asiakasmäärä, joka näytetään käyttöliittymässä.
+	    */
 		public void naytaAsiakasMaara(int C);
+		
+		/**
+	    Asettaa Check-in aktiiviajan käyttöliittymässä näytettäväksi.
+	    
+	    @param B Check-in aktiiviaika, joka näytetään käyttöliittymässä.
+	    */
 		public void checkAktiiviAika(double B);
+		
+		/**
+	    Asettaa turvatarkastuksen läpäisyajan käyttöliittymässä näytettäväksi.
+	    
+	    @param Ri Turvatarkastuksen läpäisyajan keskimääräinen arvo, joka näytetään käyttöliittymässä.
+	    */
 		public void turvaTarkastus(double Ri);
+		
+		/**
+		Asettaa oleskeluajan turvatarkastuksessa käyttöliittymässä näytettäväksi.
+		
+	    @param W Oleskeluaika turvatarkastuksessa, joka näytetään käyttöliittymässä.
+	    */
 		public void oleskeluAikaTurvaTarkastus(double W);
 
+		/**
+	    Asettaa Check-in -jonon käyttöasteen käyttöliittymässä näytettäväksi.
+	    
+	    @param K Check-in -jonon käyttöaste, joka näytetään käyttöliittymässä.
+	    */
 		public void checkInKayttoaste(double K);
+		
+		/**
+	    Asettaa lentokentän tehon käyttöliittymässä näytettäväksi.
+	    
+	    @param S Lentokentän teho, joka näytetään käyttöliittymässä.
+	    */
 		public void lentokentanTeho(double S);
+		
+		/**
+	    Asettaa Check-in -palveluajan käyttöliittymässä näytettäväksi.
+	    
+	    @param P Check-in -palveluaika, joka näytetään käyttöliittymässä.
+	    */
 		public void checkinPalveluaika(double P);
+		
+		/**
+	    Asettaa turvatarkastusjonon pituuden käyttöliittymässä näytettäväksi.
+	    
+	    @param A Turvatarkastusjonon pituus, joka näytetään käyttöliittymässä.
+	    */
 		public void turvatarkastusJono(double A);
+		
+		/**
+	    Asettaa turvatarkastusjonon odotusajan käyttöliittymässä näytettäväksi.
+	    
+	    @param R Turvatarkastusjonon odotusaika, joka näytetään käyttöliittymässä.
+	    */
 		public void turvatarkastusJononPituus(double R);
+
+		/**
+ 		* Metodi visualisoi asiakkaan nykyisen ja edellisen tapahtuman
+ 		*
+ 		* @param uusi uusi tapahtuma
+ 		* @param vanha vanha tapahtuma
+ 		*/
 		public void visualisoiAsiakas(TapahtumanTyyppi uusi, TapahtumanTyyppi vanha);
 
 }
--- src/controller/Kontrolleri.java
+++ src/controller/Kontrolleri.java
@@ -6,14 +6,29 @@ import simu.framework.IMoottori;
 import simu.model.*;
 import view.ISimulaattorinUI;
 
+/**
+Kontrolleri-luokka hallinnoi simulaattorin toimintaa ja kommunikoi käyttöliittymän ja moottorin kanssa.
+Se myös ylläpitää yhteyttä tuloskoneeseen ja tuloskantaan.
+
+@author tekijä
+@version versionumero
+*/
 public class Kontrolleri implements IKontrolleri{   // UUSI
+	
+	/**
+    Staattinen muuttuja tulosDAO, joka sisältää viittauksen tuloskantaan.
+    */
 	static ITulosDAO tulosDAO = new TulosAccessObject();
 
 	private IMoottori moottori;
 	private ITuloskone tuloskone;
 	private ISimulaattorinUI ui;
-
-
+	
+	/**
+    Kontrollerin konstruktori, joka luo uuden Kontrolleri-olion.
+    
+    @param ui Käyttöliittymä, jonka kanssa Kontrolleri kommunikoi.
+    */
 	public Kontrolleri(ISimulaattorinUI ui) {
 		this.ui = ui;
 		this.tuloskone = new Tuloskone();
@@ -23,6 +38,14 @@ public class Kontrolleri implements IKontrolleri{   // UUSI
 	// Moottorin ohjausta:
 
 	@Override
+	/**
+    Käynnistää simuloinnin
+    Luo uuden moottorisäikeen jokaista simulointia varten.
+    Asettaa säikeelle simulointiajan, viiveen, sisäänkäynnin jakautuman, check-in jakautuman
+    ja palvelupisteiden lukumäärän käyttöliittymästä.
+    Tyhjentää käyttöliittymän visualisoinnin näytön.
+    Käynnistää moottorisäikeen.
+    */
 	public void kaynnistaSimulointi() {
 		moottori = new OmaMoottori(this); // luodaan uusi moottorisäie jokaista simulointia varten
 		moottori.setSimulointiaika(ui.getAika());
@@ -32,16 +55,6 @@ public class Kontrolleri implements IKontrolleri{   // UUSI
 		moottori.setPalvelupisteetLkm(ui.getInfoLkm(), ui.getManualLkm(), ui.getAutoLkm());
 		ui.getVisualisointi().tyhjennaNaytto();
 		((Thread)moottori).start();
-		//((Thread)moottori).run(); // Ei missään tapauksessa näin. Miksi?
-//		@Override
-//		public void checkInJakauma(double checkinJakauma) {
-//			
-//		}
-//		
-//		@Override
-//		public void sisaanKayntiJakauma(double sisaankayntiJakauma) {
-//			
-//		}
 	}
 
 	@Override
@@ -59,59 +72,97 @@ public class Kontrolleri implements IKontrolleri{   // UUSI
 	// Simulointitulosten välittämistä käyttöliittymään.
 	// Koska FX-ui:n päivitykset tulevat moottorisäikeestä, ne pitää ohjata JavaFX-säikeeseen:
 
+	/**
+    @param aika loppuaika, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void naytaLoppuaika(double aika) {
 		Platform.runLater(()->ui.setLoppuaika(aika));
-
 	}
 
+	/**
+    @param C asiakasmäärä, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void naytaAsiakasMaara(int C) {
 		Platform.runLater(()->ui.setAsiakasMaara(C));
 	}
 
+	/**
+    @param B Check-in aktiiviaika, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void checkAktiiviAika(double B) {
 		Platform.runLater(()->ui.setCheckAktiiviAika(B));
 	}
 
+	/**
+    @param Ri Turvatarkastuksen läpimenoaika, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void turvaTarkastus(double Ri) {
 		Platform.runLater(()->ui.setTurvaTarkastus(Ri));
 	}
 
+	/**
+    @param W Turvatarkastuksen oleskeluaika, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void oleskeluAikaTurvaTarkastus(double W) {
 		Platform.runLater(()->ui.setOleskeluAikaTurvaTarkastus(W));
 	}
 
+	/**
+    @param K Check-in käyttöaste, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void checkInKayttoaste(double K) {
 		Platform.runLater(()->ui.setCheckInKayttoaste(K));
 	}
 
+	/**
+    @param S Lentokentän teho, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void lentokentanTeho(double S) {
 		Platform.runLater(()->ui.setLentokentanTeho(S));
 	}
 
+	/**
+    @param P Check-in palveluaika, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void checkinPalveluaika(double P) {
 		Platform.runLater(()->ui.setCheckinPalveluaika(P));
 	}
 
+	/**
+    @param A Turvatarkastuksen jono, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void turvatarkastusJono(double A) {
 		Platform.runLater(()->ui.setTurvatarkastusJono(A));
 	}
 
+	/**
+    @param R Turvatarkastuksen jonon pituus, joka asetetaan käyttöliittymässä näytettäväksi
+    */
 	@Override
 	public void turvatarkastusJononPituus(double R) {
 		Platform.runLater(()->ui.setTurvatarkastusJononPituus(R));
 	}
 
 	@Override
+	/**
+    Visualisoi uuden asiakkaan tapahtuman tapahtumatyypin uusi ja vanhan tapahtumatyypin vanha välillä.
+    
+    @param uusi uusi asiakas
+    @param vanha vanha asiakas
+    */
 	public void visualisoiAsiakas(TapahtumanTyyppi uusi, TapahtumanTyyppi vanha) {
+		/**
+	    Moottorisäie, joka käynnistetään simulaation aikana
+	    */
 		Platform.runLater(new Runnable(){
 			@Override
 			public void run(){
@@ -128,15 +179,7 @@ public class Kontrolleri implements IKontrolleri{   // UUSI
 	@Override
 	public String[] naytaTulokset() {
 		this.tuloskone = new Tuloskone();
-//		String[] t = tuloskone.getTulokset();
-//		
-//		for(String v:t ) {
-//			System.out.println(v + "Näytä tulokset");
-//			
-//		}
-//		System.out.println("");
-		
-		
+	
 		return tuloskone.getTulokset();
 		
 		
